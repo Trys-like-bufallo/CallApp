50e1079dcab1a84e4d7b87643bf6936a59e683c6
hoài an trần <hoaian412003@gmail.com>
2021-11-11 21:45:59 +0700

    123

--- server/Controller/getdataHandle.js
+++
@@ -1,8 +0,0 @@
-import inforvs from '../MongoDB/Schema/inforvsSchema.js';
-
-const getdataHandle = (req, res) => {
-    inforvs.find({})
-    .then(data => res.send(data));
-}
-
-export default getdataHandle;
--- server/MongoDB/Schema/sessionSchema.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const {Schema} = mongoose;
-
-const sessionSchema = new Schema({
-    lastUse: {type: Date, default: new Date()},
-});
-
-const session = mongoose.model('session', sessionSchema);
-export default session;
